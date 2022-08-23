412d6113ea442db2dae3991071806b45fddb463c
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-08-23 13:00:09 +0100

    Create App.test.js

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import App from '../App';
+import EventList from '../EventList';
+import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
+
+import { mockData } from '../mock-data';
+import { extractLocations, getEvents } from '../api';
+
+// Unit tests
+describe('<App /> component', () => {
+    let AppWrapper;
+    beforeAll(() => {
+        AppWrapper = shallow(<App />);
+    });
+
+    test('render list of events', () => {
+        expect(AppWrapper.find(EventList)).toHaveLength(1);
+    });
+
+    test('render CitySearch', () => {
+        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+    });
+
+    test('render NumberOfEvents', () => {
+        expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+    });
+});
+
+// Integration tests
+describe('<App /> integration', () => {
+    /*let AppWrapper;
+    beforeEach(() => {
+        AppWrapper = mount(<App />);
+    })*/
+
+    test('App passes "events" state as a prop to EventList', () => {
+        const AppWrapper = mount(<App />);
+        const AppEventsState = AppWrapper.state('events');
+        expect(AppEventsState).not.toEqual(undefined);
+        expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+        AppWrapper.unmount();
+    });
+
+    test('App passes "locations" state as a prop to CitySearch', () => {
+        const AppWrapper = mount(<App />);
+        const AppLocationsState = AppWrapper.state('locations');
+        expect(AppLocationsState).not.toEqual(undefined);
+        expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+        AppWrapper.unmount();
+    });
+
+    test('get list of events matching the city selected by the user', async () => {
+        const AppWrapper = mount(<App />);
+        const CitySearchWrapper = AppWrapper.find(CitySearch);
+        const locations = extractLocations(mockData);
+        CitySearchWrapper.setState({ suggestions: locations });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        const selectedIndex = Math.floor(Math.random() * (suggestions.length));
+        const selectedCity = suggestions[selectedIndex];
+        await CitySearchWrapper.instance().handleItemClicked(selectedCity);
+        const allEvents = await getEvents();
+        const eventsToShow = allEvents.filter(event => event.location === selectedCity);
+        expect(AppWrapper.state('events')).toEqual(eventsToShow.slice(0, 32));
+        AppWrapper.unmount();
+    });
+
+    test('get list of all events when user selects "See all cities"', async () => {
+        const AppWrapper = mount(<App />);
+        const suggestionItems = AppWrapper.find(CitySearch).find('.suggestions li');
+        await suggestionItems.at(suggestionItems.length - 1).simulate('click');
+        const allEvents = await getEvents();
+        expect(AppWrapper.state('events')).toEqual(allEvents.slice(0, 32));
+        AppWrapper.unmount();
+    });
+
+    test('App passes "numberOfEvents" state as a prop to NumberOfEvents', () => {
+        const AppWrapper = mount(<App />);
+        const AppNumberState = AppWrapper.state('numberOfEvents');
+        expect(AppNumberState).not.toEqual(undefined);
+        expect(AppWrapper.find(NumberOfEvents).props().numberOfEvents).toEqual(AppNumberState);
+        AppWrapper.unmount();
+    });
+
+    test('get list of events matching the number of events inputted by the user', async () => {
+        const AppWrapper = mount(<App />);
+        const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+        const number = Math.floor(Math.random() * (32));
+        const eventObject = { target: { value: number } };
+        await NumberOfEventsWrapper.find('.render-number').simulate('change', eventObject);
+        expect(AppWrapper.state('numberOfEvents')).toEqual(number);
+        AppWrapper.unmount();
+    });
+});
