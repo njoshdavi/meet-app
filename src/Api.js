35282a6c0edf7bf7dab848e97c66eb164c5c164f
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-08-30 15:23:48 +0100

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,157 +1,105 @@
-import React, { Component } from "react";
-import "./App.css";
-import EventList from "./EventList";
-import EventGenre from "./EventGenre";
-import CitySearch from "./CitySearch";
-import NumberOfEvents from "./NumberOfEvents";
-import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
-import { OffLineAlert } from "./Alert";
-import "./nprogress.css";
-import WelcomeScreen from "./WelcomeScreen";
-import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
+import { mockData } from "./mock-data";
+import axios from "axios";
+import NProgress from "nprogress";
 
-class App extends Component {
-  state = {
-    events: [],
-    locations: [],
-    numberOfEvents: 32,
-    locationSelected: "all",
-    showWelcomeScreen: undefined,
-  };
+export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
+};
 
-  async componentDidMount() {
-    this.mounted = true;
-    const accessToken = localStorage.getItem("access_token");
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          let sliceNumber = this.state.numberOfEvents;
-          this.setState({
-            locations: extractLocations(events),
-            events: events.slice(0, sliceNumber),
-          });
-        }
-      });
-    }
-  }
+export const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+  return result;
+};
 
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  updateEvents = (location, maxNumberEvents) => {
-    if (maxNumberEvents === undefined) {
-      maxNumberEvents = this.state.numberOfEvents;
-    } else this.setState({ numberOfEvents: maxNumberEvents });
-    if (location === undefined) {
-      location = this.state.locationSelected;
-    }
-    getEvents().then((events) => {
-      let locationEvents =
-        location === "all"
-          ? events
-          : events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents.slice(0, maxNumberEvents),
-        numberOfEvents: maxNumberEvents,
-        locationSelected: location,
-      });
-    });
-  };
-
-  updateNumberEvents = (numberOfEvents) => {
-    this.setState({
-      numberOfEvents,
-    });
-    this.updateEvents(undefined, numberOfEvents);
-  };
-
-  getData = () => {
-    const { locations, events } = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter(
-        (event) => event.location === location
-      ).length;
-      const city = location.split(", ").shift();
-      return { city, number };
-    });
-    return data;
-  };
-
-  render() {
-    if (this.state.showWelcomeScreen === undefined)
-      return <div className="App" />;
-    return (
-      <div className="App">
-        <WelcomeScreen
-          showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => {
-            getAccessToken();
-          }}
-        />
-        <div className="offlineAlert">
-          {!navigator.onLine && (
-            <OffLineAlert text={"You are currently offline!"} />
-          )}
-        </div>
-
-        <h1>Come and see what's going on</h1>
-
-        <CitySearch
-          locations={this.state.locations}
-          updateEvents={this.updateEvents}
-        />
-        <NumberOfEvents
-          updateEvents={this.updateEvents}
-          numberOfEvents={this.state.numberOfEvents}
-        />
-        <div className="data-vis-wrapper">
-          <div className="pie-wrapper">
-            <EventGenre events={this.state.events} />
-          </div>
-          <div className="scatter-wrapper">
-            <ResponsiveContainer>
-              <ScatterChart
-                width={400}
-                height={400}
-                margin={{
-                  top: 20,
-                  right: 20,
-                  bottom: 20,
-                  left: 20,
-                }}
-              >
-                <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City" />
-                <YAxis
-                  type="number"
-                  dataKey="number"
-                  name="Number of events"
-                  allowDecimals={false}
-                />
-
-                <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-                <Scatter data={this.getData()} fill="#8884d8" />
-              </ScatterChart>
-            </ResponsiveContainer>
-          </div>
-        </div>
-        <EventList events={this.state.events} />
-      </div>
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+      "https://4lnd75qgye.execute-api.eu-central-1.amazonaws.com/dev/api/token" +
+        "/" +
+        encodeCode
     );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    error.json();
   }
-}
+};
 
-export default App;
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://4lnd75qgye.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+
+  return accessToken;
+};
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+export const getEvents = async () => {
+  NProgress.start();
+
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url =
+      "https://4lnd75qgye.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
+      "/" +
+      token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
+};
