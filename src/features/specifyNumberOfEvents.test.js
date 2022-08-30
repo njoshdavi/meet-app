f7ca1aa81d05f51225bca57e13341243de5b2789
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-08-30 13:58:42 +0100

    Update specifyNumberOfEvents.test.js

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,44 +1,68 @@
-import { loadFeature, defineFeature } from 'jest-cucumber';
-import React from 'react';
-import { mount } from 'enzyme';
+import { mount } from "enzyme";
+import { loadFeature, defineFeature } from "jest-cucumber";
+import React from "react";
+import { extractLocations } from "../api";
+import App from "../App";
+import { mockData } from "../mock-data";
+import NumberOfEvents from "../NumberOfEvents";
 
-import App from '../App';
-import NumberOfEvents from '../NumberOfEvents';
+const feature = loadFeature("./src/features/specifyNumberOfEvents.feature");
+const locations = extractLocations(mockData);
 
-const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
+defineFeature(feature, (test) => {
+  <></>;
 
-defineFeature(feature, test => {
-    test('When user hasn’t specified a number, 32 is the default number', ({ given, when, then }) => {
-        given('the app has not yet been opened', () => {
+  let AppWrapper;
+  let NumberOfEventsWrapper;
 
-        });
-
-        let AppWrapper;
-        when('the user opens the app', () => {
-            AppWrapper = mount(<App />);
-        });
-
-        then('32 events should be shown on screen', () => {
-            AppWrapper.update();
-            expect(AppWrapper.state('numberOfEvents')).toEqual(32);
-        });
+  // Scenario 1
+  test("When the user hasn’t specified a number, 32 is the default number.", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("the user at the home page", async () => {
+      AppWrapper = await mount(<App />);
     });
 
-    test('User can change the number of events they want to see', ({ given, when, then }) => {
-        let AppWrapper;
-        given('the main page is open', () => {
-            AppWrapper = mount(<App />);
-        });
-
-        when('the user enters a number (e.g., 10)', () => {
-            AppWrapper.update();
-            let NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
-            const eventObject = { target: { value: 10 } };
-            NumberOfEventsWrapper.find('.render-number').simulate('change', eventObject);
-        });
-
-        then('that number of events (i.e., 10) should be shown on screen', () => {
-            expect(AppWrapper.state('numberOfEvents')).toBe(10);
-        });
+    when("the user chooses no specific amount of search results", () => {
+      AppWrapper.update();
     });
+
+    then(
+      "the default amount of visible search results per city will be 32",
+      () => {
+        expect(AppWrapper.find(".event-visible")).toHaveLength(2);
+      }
+    );
+  });
+
+  // Scenario 2
+  test("The user can change the number of events they want to see.", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("the user opened the search results query", () => {
+      AppWrapper.update();
+      expect(AppWrapper.find(".event-visible")).toHaveLength(2);
+    });
+
+    when("the user changes the default number", () => {
+      const eventNumber = { target: { value: 1 } };
+      NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+      NumberOfEventsWrapper.setState({ events: locations, eventCount: 1 });
+      NumberOfEventsWrapper.find(".default").simulate("change", eventNumber);
+      expect(AppWrapper.find(".default").props().value).toEqual(1);
+      AppWrapper.update();
+    });
+
+    then(
+      "the default number of results will be changed to what the users select",
+      () => {
+        expect(NumberOfEventsWrapper.state("numberOfEvents")).toBe(1);
+        AppWrapper.unmount();
+      }
+    );
+  });
 });
