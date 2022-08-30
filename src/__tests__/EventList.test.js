45408948dc4f17ab199a8b0f8a0612cf8a084ce1
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-08-30 12:37:56 +0100

    Update EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
+import EventList from "../EventList";
+import { mockData } from "../mock-data";
+import Event from "../Event";
 
-import { mockData } from '../mock-data';
-
-import EventList from '../EventList';
-import Event from '../Event';
-
-describe('<EventList /> component', () => {
-    test('render correct number of events', () => {
-        const EventListWrapper = shallow(<EventList events={mockData} />);
-        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
-    });
+describe("<EventList /> component", () => {
+  test("render correct number of events", () => {
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
+  });
 });
