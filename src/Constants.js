f810bcc3f900f4668e28745b6ee2df43f3f8084c
David <njoshdavi@gmail.com>
2022-09-08 01:20:10 +0300

    updates

---
+++ src/Constants.js
@@ -0,0 +1,3 @@
+export const PRIVACY_URL = 'https://rswager83.github.io/meet_/privacy.html';
+export const GOOGLE_LOGO_URL = 'https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg';
+export const AUTH_BASE_API = 'https://2swcxgimr2.execute-api.eu-central-1.amazonaws.com/';
